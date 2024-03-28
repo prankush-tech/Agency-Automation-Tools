e0e3f82841f0aa7daf75f9fe28dd3231e0b10b88
Prankush Giri <prankushgiri@.com>
2024-03-29 03:08:21 +0530

    ✅2 section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { BentoGridSecondDemo } from '@/components/global/bentoGrid';
 
 import Button from '@/components/global/ui/button';
 import { CardHoverEffectDemo } from '@/components/global/cardGrid';
+import { Pricing } from '@/components/global/pricing';
 
 export default function Home() {
 	return (
@@ -11,12 +12,12 @@ export default function Home() {
 			<Navbar />
 			<section className="relative p-2 md:p-10 mt-24">
 				<h1 className="text-center text-3xl md:text-5xl lg:text-9xl font-bold mt-24 dark:text-neutral-50">
-					ZENCY AGENCY MANAGEMENT
+					ZENCY-AGENCY MANAGEMENT
 				</h1>
 				<p className="text-center text-neutral-600 m-2 md:text-xl md:m-6 lg:mb-24 dark:text-neutral-400">
 					Manage all your agency work in one place. With absolute Automation
 				</p>
-				<Button />
+				<Button text={"GET STARTED"} />
 				<BentoGridSecondDemo />
 			</section>
 
@@ -26,7 +27,10 @@ export default function Home() {
 
 			<section>
 			<CardHoverEffectDemo/>
+			<Button text={"EXPLORE"} />
 			</section>
+
+		
 		</main>
 	);
 }
--- src/components/global/ui/button.tsx
+++ src/components/global/ui/button.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+    text:string
+}
 
 const Button = (props: Props) => {
     return (
         <div className="w-full flex justify-center items-center pb-12">
 
         <button className="px-24 py-6 rounded-full bg-[#e2f24b] font-bold text-neutral-900 text-xl tracking-widest uppercase transform hover:bg-neutral-900 hover:text-neutral-50 transition-colors duration-400 dark:hover:bg-neutral-50 dark:hover:text-neutral-900">
-            GET STARTED
+            {props.text}
         </button>
 
 
