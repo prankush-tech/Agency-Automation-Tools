999f24e8c84ef5d58eafe32f36f71a7b19f70172
Prankush Giri <prankushgiri@gmail.com>
2024-09-03 06:52:57 +0530

    server Cooldown fix🎃🎃

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,26 +1,17 @@
 import { db } from "@/lib/database";
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from "next/server";
 
-
-
 export async function GET() {
-    try {
+  try {
+    const users = await db.ping.findMany();
+    console.log("Users:", users);
 
-        const users = await db.ping.findMany();
-        console.log('Users:', users);
-
-  
-        return NextResponse.json(users)
-        
-    } 
-    catch (error) 
-    {
-        return NextResponse.json({
-            error:Error
-        })
-    } finally {
-      await db.$disconnect();
-    }
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json({
+      error: Error,
+    });
+  } finally {
+    await db.$disconnect();
   }
-  
+}
